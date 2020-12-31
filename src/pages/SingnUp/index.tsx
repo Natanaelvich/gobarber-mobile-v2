8dd266d0788bde483880939a311a8276867446e0
Natanael Lima <taelima1997@gmail.com>
2020-12-31 14:00:00 +0100

    delete imports unsed

--- src/pages/SingnUp/index.tsx
+++ src/pages/SingnUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useEffect } from 'react';
+import React, { useRef, useCallback } from 'react';
 
 import { Feather } from 'expo-vector-icons';
 import * as Yup from 'yup';
