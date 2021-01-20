8e8e0334b3bc5393370e8159a87e89d0f33c56c8
Natanael Lima <taelima1997@gmail.com>
2021-01-20 00:16:09 -0300

    delete margins and bottom paddiing

--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,10 +1,9 @@
 import styled from 'styled-components/native';
-import { Platform } from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -18,6 +17,5 @@ export const Avatar = styled.Image`
   width: 186px;
   height: 186px;
   border-radius: 98px;
-  margin-top: 64px;
   align-self: center;
 `;
