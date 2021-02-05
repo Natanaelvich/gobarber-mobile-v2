7fa62ec4e522c41f3adf4f3308342d43d35dc907
Natanael Lima <taelima1997@gmail.com>
2021-02-04 23:28:20 -0300

    integrate bagsnag

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import Bugsnag from '@bugsnag/expo';
+import { Text, View } from 'react-native';
 import Main from './src';
 
+Bugsnag.start();
+
+const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
+
 const App: React.FC = () => {
-  return <Main />;
+  return (
+    <ErrorBoundary
+      FallbackComponent={() => (
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Text>Tem um erro oh</Text>
+        </View>
+      )}
+    >
+      <Main />
+    </ErrorBoundary>
+  );
 };
 
 export default App;
