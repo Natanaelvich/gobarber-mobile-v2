c3ab145fea6c7b623b3e3e6822d6405c52293a11
Natanael Lima <taelima1997@gmail.com>
2021-02-14 23:25:32 -0300

    create tests appointmentsCreated page

--- src/__test__/pages/AppointmentCreated.spec.tsx
+++ src/__test__/pages/AppointmentCreated.spec.tsx
@@ -1,18 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import SingnIn from '../../pages/SingnIn';
+import { fireEvent, render } from '@testing-library/react-native';
+import AppointmentCreated from '../../pages/AppointmentCreated';
+
+const mockedReset = jest.fn();
 
 jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
   return {
-    useNavigation: jest.fn(),
+    ...actualNav,
+    useNavigation: () => ({
+      reset: mockedReset,
+    }),
+    useRoute: () => ({
+      params: {
+        date: new Date('2021-02-14 23:16:22'),
+      },
+    }),
   };
 });
 
 describe('SignIn page', () => {
-  it('Should contains email/password inputs', () => {
-    const { getByPlaceholderText } = render(<SingnIn />);
+  it('Should be able see page appointment created', () => {
+    const { getByText } = render(<AppointmentCreated />);
 
-    expect(getByPlaceholderText('E-mail')).toBeTruthy();
-    expect(getByPlaceholderText('Senha')).toBeTruthy();
+    expect(
+      getByText('domingo, dia 14 de fevereiro de 2021 às 23:16h'),
+    ).toBeTruthy();
+  });
+
+  it('Should be able navigate to Dashboard', () => {
+    mockedReset.mockImplementation(options => options);
+
+    const { getByText } = render(<AppointmentCreated />);
+
+    const buttonElement = getByText('Ok');
+
+    fireEvent.press(buttonElement);
+
+    expect(mockedReset).toBeCalledWith({
+      index: 0,
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+    });
   });
 });
