f256b7da48c27e189a53827162d05d4cc0626d6a
Natanael Lima <taelima1997@gmail.com>
2021-06-12 20:18:48 -0300

    add reanimated setup tests

--- src/__test__/jestSetup.ts
+++ src/__test__/jestSetup.ts
@@ -1,3 +1,5 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+require('react-native-reanimated/src/reanimated2/jestUtils').setUpTests();
+
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
