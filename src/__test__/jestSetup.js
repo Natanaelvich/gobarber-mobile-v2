29d57de7e158cae6d72626a9b2b9e4e39ffa969e
Natanael Lima <taelima1997@gmail.com>
2021-06-30 13:17:54 -0300

    add mock reanimated

--- src/__test__/jestSetup.js
+++ src/__test__/jestSetup.js
@@ -3,3 +3,24 @@ import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/asy
 require('react-native-reanimated/src/reanimated2/jestUtils').setUpTests();
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+global.__reanimatedWorkletInit = jest.fn();
+
+jest.mock('react-native-reanimated', () => {
+  return {
+    // @ts-ignore
+    ...jest.requireActual('react-native-reanimated/mock'),
+    useSharedValue: jest.fn,
+    useAnimatedStyle: jest.fn,
+    withTiming: jest.fn,
+    withSpring: jest.fn,
+    withRepeat: jest.fn,
+    withSequence: jest.fn,
+    useAnimatedProps: jest.fn,
+    Easing: {
+      linear: jest.fn,
+      elastic: jest.fn,
+      out : jest.fn
+    },
+  };
+});
